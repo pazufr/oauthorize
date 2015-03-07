9cbb59cb6b1b243c38d10bb6f52ac526185cbe89
Łukasz Gurdek <ukasiu@users.noreply.github.com>
2015-03-07 15:47:58 +0100

    Exit after redirect

--- root/includes/hooks/hook_oauthorize.php
+++ root/includes/hooks/hook_oauthorize.php
@@ -18,6 +18,7 @@ class OauthLogin
      global $template, $user, $phpbb_root_path, $phpEx, $config, $mode;
      if($mode == 'logout')
         header('Location: https://accounts.iiet.pl/students/sign_out');
+        exit();
     
     // We get the content from the normal display function 
     // and modify/enrich it
